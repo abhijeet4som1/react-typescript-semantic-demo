84171fa377047766f003d45103b04be8c1641149
Abhi <abhijeet4som1@gmail.com>
2017-02-03 20:31:25 +0530

    adding typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
